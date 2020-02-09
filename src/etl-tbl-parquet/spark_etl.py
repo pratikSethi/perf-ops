94f4f049af86557f36960ca0ada5c694c71859fe
Pratik Sethi <pratik.root@gmail.com>
2020-02-09 11:25:25 -0800

    added new source and sink for 100 GB dataset

--- src/etl-tbl-parquet/spark_etl.py
+++ src/etl-tbl-parquet/spark_etl.py
@@ -27,55 +27,29 @@ FS_S3_IMPL_CLASSNAME = 'org.apache.hadoop.fs.s3native.NativeS3FileSystem'
 FS_S3_AWS_ACCESS_KEY_ID_KEY = 'fs.s3n.awsAccessKeyId'
 FS_S3_AWS_SECRET_ACCESS_KEY_KEY = 'fs.s3n.awsSecretAccessKey'
 
-# Sample Unprocessed datasets
-SMALL_SAMPLE_UNPROCESSED_DATASET_ROOT_URL = 's3a://sample-unprocessed/tpch/block/1/*'
-# S3 Locations 10 GB TPCH Dataset
-MEDIUM_SAMPLE_UNPROCESSED_DATASET_ROOT_URL = 's3a://sample-unprocessed/tpch/block/10/*'
-
 # TODO: Maybe create a constant map SMALL_SAMPLE_UNPROCESSED_DATASET = {'Customer' : <s3-url>}
 # So that we can automate the loading or maybe use some wildcards trick to automate the stuff
-# ================ SMALL UNPROCESSED TABLES ================ #
-SMALL_SAMPLE_UNPROCESSED_DATASET_CUSTOMER_ROOT_URL = 's3a://sample-unprocessed/tpch/block/1/customer.tbl'
-SMALL_SAMPLE_UNPROCESSED_DATASET_LINEITEM_ROOT_URL = 's3a://sample-unprocessed/tpch/block/1/lineitem.tbl'
-SMALL_SAMPLE_UNPROCESSED_DATASET_NATION_ROOT_URL = 's3a://sample-unprocessed/tpch/block/1/nation.tbl'
-SMALL_SAMPLE_UNPROCESSED_DATASET_ORDER_ROOT_URL = 's3a://sample-unprocessed/tpch/block/1/orders.tbl'
-SMALL_SAMPLE_UNPROCESSED_DATASET_PART_ROOT_URL = 's3a://sample-unprocessed/tpch/block/1/part.tbl'
-SMALL_SAMPLE_UNPROCESSED_DATASET_PARTSUPP_ROOT_URL = 's3a://sample-unprocessed/tpch/block/1/partsupp.tbl'
-SMALL_SAMPLE_UNPROCESSED_DATASET_REGION_ROOT_URL = 's3a://sample-unprocessed/tpch/block/1/region.tbl'
-SMALL_SAMPLE_UNPROCESSED_DATASET_SUPPLIER_ROOT_URL = 's3a://sample-unprocessed/tpch/block/1/supplier.tbl'
-# ================ SMALL UNPROCESSED TABLES ================ #
-
-# ================ SMALL PROCESSED TABLES ================ #
-SMALL_SAMPLE_PROCESSED_DATASET_CUSTOMER_ROOT_URL = 's3a://sample-processed/tpch/block/1/customer/'
-SMALL_SAMPLE_PROCESSED_DATASET_LINEITEM_ROOT_URL = 's3a://sample-processed/tpch/block/1/lineitem/'
-SMALL_SAMPLE_PROCESSED_DATASET_NATION_ROOT_URL = 's3a://sample-processed/tpch/block/1/nation/'
-SMALL_SAMPLE_PROCESSED_DATASET_ORDER_ROOT_URL = 's3a://sample-processed/tpch/block/1/orders/'
-SMALL_SAMPLE_PROCESSED_DATASET_PART_ROOT_URL = 's3a://sample-processed/tpch/block/1/part/'
-SMALL_SAMPLE_PROCESSED_DATASET_PARTSUPP_ROOT_URL = 's3a://sample-processed/tpch/block/1/partsupp/'
-SMALL_SAMPLE_PROCESSED_DATASET_REGION_ROOT_URL = 's3a://sample-processed/tpch/block/1/region/'
-SMALL_SAMPLE_PROCESSED_DATASET_SUPPLIER_ROOT_URL = 's3a://sample-processed/tpch/block/1/supplier/'
-# ================ SMALL PROCESSED TABLES ================ #
 
 # ================ MEDIUM UNPROCESSED TABLES ================ #
-MEDIUM_SAMPLE_UNPROCESSED_DATASET_CUSTOMER_ROOT_URL = 's3a://sample-unprocessed/tpch/block/10/customer.tbl'
-MEDIUM_SAMPLE_UNPROCESSED_DATASET_LINEITEM_ROOT_URL = 's3a://sample-unprocessed/tpch/block/10/lineitem.tbl'
-MEDIUM_SAMPLE_UNPROCESSED_DATASET_NATION_ROOT_URL = 's3a://sample-unprocessed/tpch/block/10/nation.tbl'
-MEDIUM_SAMPLE_UNPROCESSED_DATASET_ORDER_ROOT_URL = 's3a://sample-unprocessed/tpch/block/10/orders.tbl'
-MEDIUM_SAMPLE_UNPROCESSED_DATASET_PART_ROOT_URL = 's3a://sample-unprocessed/tpch/block/10/part.tbl'
-MEDIUM_SAMPLE_UNPROCESSED_DATASET_PARTSUPP_ROOT_URL = 's3a://sample-unprocessed/tpch/block/10/partsupp.tbl'
-MEDIUM_SAMPLE_UNPROCESSED_DATASET_REGION_ROOT_URL = 's3a://sample-unprocessed/tpch/block/10/region.tbl'
-MEDIUM_SAMPLE_UNPROCESSED_DATASET_SUPPLIER_ROOT_URL = 's3a://sample-unprocessed/tpch/block/10/supplier.tbl'
+MEDIUM_SAMPLE_UNPROCESSED_DATASET_CUSTOMER_ROOT_URL = 's3a://optmark-unprocessed/tpch/block/100/customer.tbl'
+MEDIUM_SAMPLE_UNPROCESSED_DATASET_LINEITEM_ROOT_URL = 's3a://optmark-unprocessed/tpch/block/100/lineitem.tbl'
+MEDIUM_SAMPLE_UNPROCESSED_DATASET_NATION_ROOT_URL = 's3a://optmark-unprocessed/tpch/block/100/nation.tbl'
+MEDIUM_SAMPLE_UNPROCESSED_DATASET_ORDER_ROOT_URL = 's3a://optmark-unprocessed/tpch/block/100/orders.tbl'
+MEDIUM_SAMPLE_UNPROCESSED_DATASET_PART_ROOT_URL = 's3a://optmark-unprocessed/tpch/block/100/part.tbl'
+MEDIUM_SAMPLE_UNPROCESSED_DATASET_PARTSUPP_ROOT_URL = 's3a://optmark-unprocessed/tpch/block/100/partsupp.tbl'
+MEDIUM_SAMPLE_UNPROCESSED_DATASET_REGION_ROOT_URL = 's3a://optmark-unprocessed/tpch/block/100/region.tbl'
+MEDIUM_SAMPLE_UNPROCESSED_DATASET_SUPPLIER_ROOT_URL = 's3a://optmark-unprocessed/tpch/block/100/supplier.tbl'
 # ================ MEDIUM UNPROCESSED TABLES ================ #
 
 # ================ MEDIUM PROCESSED TABLES ================ #
-MEDIUM_SAMPLE_PROCESSED_DATASET_CUSTOMER_ROOT_URL = 's3a://sample-processed/tpch/block/10/customer/'
-MEDIUM_SAMPLE_PROCESSED_DATASET_LINEITEM_ROOT_URL = 's3a://sample-processed/tpch/block/10/lineitem/'
-MEDIUM_SAMPLE_PROCESSED_DATASET_NATION_ROOT_URL = 's3a://sample-processed/tpch/block/10/nation/'
-MEDIUM_SAMPLE_PROCESSED_DATASET_ORDER_ROOT_URL = 's3a://sample-processed/tpch/block/10/orders/'
-MEDIUM_SAMPLE_PROCESSED_DATASET_PART_ROOT_URL = 's3a://sample-processed/tpch/block/10/part/'
-MEDIUM_SAMPLE_PROCESSED_DATASET_PARTSUPP_ROOT_URL = 's3a://sample-processed/tpch/block/10/partsupp/'
-MEDIUM_SAMPLE_PROCESSED_DATASET_REGION_ROOT_URL = 's3a://sample-processed/tpch/block/10/region/'
-MEDIUM_SAMPLE_PROCESSED_DATASET_SUPPLIER_ROOT_URL = 's3a://sample-processed/tpch/block/10/supplier/'
+MEDIUM_SAMPLE_PROCESSED_DATASET_CUSTOMER_ROOT_URL = 's3a://optmark-processed/tpch/block/100/customer/'
+MEDIUM_SAMPLE_PROCESSED_DATASET_LINEITEM_ROOT_URL = 's3a://optmark-processed/tpch/block/100/lineitem/'
+MEDIUM_SAMPLE_PROCESSED_DATASET_NATION_ROOT_URL = 's3a://optmark-processed/tpch/block/100/nation/'
+MEDIUM_SAMPLE_PROCESSED_DATASET_ORDER_ROOT_URL = 's3a://optmark-processed/tpch/block/100/orders/'
+MEDIUM_SAMPLE_PROCESSED_DATASET_PART_ROOT_URL = 's3a://optmark-processed/tpch/block/100/part/'
+MEDIUM_SAMPLE_PROCESSED_DATASET_PARTSUPP_ROOT_URL = 's3a://optmark-processed/tpch/block/100/partsupp/'
+MEDIUM_SAMPLE_PROCESSED_DATASET_REGION_ROOT_URL = 's3a://optmark-processed/tpch/block/100/region/'
+MEDIUM_SAMPLE_PROCESSED_DATASET_SUPPLIER_ROOT_URL = 's3a://optmark-processed/tpch/block/100/supplier/'
 # ================ MEDIUM PROCESSED TABLES ================ #
 
 
@@ -133,83 +107,6 @@ def main():
     # sampleBorderSchema = schema.TPCHSchema().getSampleBorderSchema()
 
     # TODO: Automate this maybe using a for loop or some other profile config param
-    # 1 GB Datasets Trasnformations =====================================================
-    '''
-    ### Customer
-    df_customer = sqlContext.read \
-        .format('com.databricks.spark.csv') \
-        .options(header='false') \
-        .options(delimiter=PIPE_DELIMITER) \
-        .load(SMALL_SAMPLE_UNPROCESSED_DATASET_CUSTOMER_ROOT_URL, schema=customerSchema)
-
-    df_customer.write.parquet(SMALL_SAMPLE_PROCESSED_DATASET_CUSTOMER_ROOT_URL)
-
-    ### Lineitem
-    df_lineitem = sqlContext.read \
-        .format('com.databricks.spark.csv') \
-        .options(header='false') \
-        .options(delimiter=PIPE_DELIMITER) \
-        .load(SMALL_SAMPLE_UNPROCESSED_DATASET_LINEITEM_ROOT_URL, schema=lineitemSchema)
-
-    df_lineitem.write.parquet(SMALL_SAMPLE_PROCESSED_DATASET_LINEITEM_ROOT_URL)
-
-    ### Nation
-    df_nation = sqlContext.read \
-        .format('com.databricks.spark.csv') \
-        .options(header='false') \
-        .options(delimiter=PIPE_DELIMITER) \
-        .load(SMALL_SAMPLE_UNPROCESSED_DATASET_NATION_ROOT_URL, schema=nationSchema)
-
-    df_nation.write.parquet(SMALL_SAMPLE_PROCESSED_DATASET_NATION_ROOT_URL)
-
-    ### Order
-    df_order = sqlContext.read \
-        .format('com.databricks.spark.csv') \
-        .options(header='false') \
-        .options(delimiter=PIPE_DELIMITER) \
-        .load(SMALL_SAMPLE_UNPROCESSED_DATASET_ORDER_ROOT_URL, schema=orderSchema)
-
-    df_order.write.parquet(SMALL_SAMPLE_PROCESSED_DATASET_ORDER_ROOT_URL)
-
-    ### Part
-    df_part = sqlContext.read \
-        .format('com.databricks.spark.csv') \
-        .options(header='false') \
-        .options(delimiter=PIPE_DELIMITER) \
-        .load(SMALL_SAMPLE_UNPROCESSED_DATASET_PART_ROOT_URL, schema=partSchema)
-
-    df_part.write.parquet(SMALL_SAMPLE_PROCESSED_DATASET_PART_ROOT_URL)
-
-    ### Partsupp
-    df_partsupp = sqlContext.read \
-        .format('com.databricks.spark.csv') \
-        .options(header='false') \
-        .options(delimiter=PIPE_DELIMITER) \
-        .load(SMALL_SAMPLE_UNPROCESSED_DATASET_PARTSUPP_ROOT_URL, schema=partsuppSchema)
-
-    df_partsupp.write.parquet(SMALL_SAMPLE_PROCESSED_DATASET_PARTSUPP_ROOT_URL)
-
-    ### Region
-    df_region = sqlContext.read \
-        .format('com.databricks.spark.csv') \
-        .options(header='false') \
-        .options(delimiter=PIPE_DELIMITER) \
-        .load(SMALL_SAMPLE_UNPROCESSED_DATASET_REGION_ROOT_URL, schema=regionSchema)
-
-    df_region.write.parquet(SMALL_SAMPLE_PROCESSED_DATASET_REGION_ROOT_URL)
-
-    ### Supplier
-    df_supplier = sqlContext.read \
-        .format('com.databricks.spark.csv') \
-        .options(header='false') \
-        .options(delimiter=PIPE_DELIMITER) \
-        .load(SMALL_SAMPLE_UNPROCESSED_DATASET_SUPPLIER_ROOT_URL, schema=supplierSchema)
-
-    df_supplier.write.parquet(SMALL_SAMPLE_PROCESSED_DATASET_SUPPLIER_ROOT_URL)
-
-    '''
-    # 1 GB Datasets Trasnformations End =====================================================
-
     # 10 GB Datasets Trasnformations Start =====================================================
 
     # Customer
