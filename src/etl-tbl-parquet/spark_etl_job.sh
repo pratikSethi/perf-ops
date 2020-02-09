6f15d551659fb804746f1ad5a1443391a38e56b5
Pratik Sethi <pratik.root@gmail.com>
2020-02-09 11:31:12 -0800

    Fixed the etl job path on the master

--- src/etl-tbl-parquet/spark_etl_job.sh
+++ src/etl-tbl-parquet/spark_etl_job.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/spark/bin/spark-submit --master spark://10.0.0.6:7077 --packages com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.2 /home/ubuntu/stash/perf-ops/src/spark_etl.py
+/usr/local/spark/bin/spark-submit --master spark://10.0.0.6:7077 --packages com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.2 /home/ubuntu/stash/perf-ops/src/etl-tbl-parquet/spark_etl.py
